5826ee5a3640ee27032969ebd5c4dafdeca0490d
venkat sai <venky6572@gmail.com>
2023-07-12 17:58:04 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,7 @@ function Home(){
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" >
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="500">
-      <img src="./himg1.jpg" class="d-block w-100" alt="..."/>
+      <img src="https://e0.pxfuel.com/wallpapers/804/502/desktop-wallpaper-top-10-most-visited-tourist-places-in-india-popular-indian-tourism.jpg" class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item" data-bs-interval="500">
       <img src="./himg0.webp" class="d-block w-100" alt="..."/>
